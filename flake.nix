7bfbfe9ce10beef8ec1dc39dce03d4a43ef52450
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 12:21:03 +0200

    fix merge of v8.0.0

--- flake.nix
+++ flake.nix
@@ -1,41 +1,31 @@
 {
   description = "Tutorial for humanoid path planner platform";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-tutorial = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./Media
-                  ./meshes
-                  ./package.xml
-                  ./rviz
-                  ./script
-                  ./src
-                  ./srdf
-                  ./urdf
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-tutorial = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./Media
+              ./meshes
+              ./package.xml
+              ./rviz
+              ./script
+              ./src
+              ./srdf
+              ./urdf
+            ];
+          };
+        };
       }
     );
 }
