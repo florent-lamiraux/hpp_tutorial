7bf72b43b05bbc086269576df99162d0d8132b71
Florent Lamiraux <florent@laas.fr>
2026-09-04 15:12:02 +0200

    [mfja] Improve two_gears.py
      Add a path projector,
      Add a constraint of linear motion on the second gear
      Increase number of loops of random shortcut.

--- mfja/two_gears.py
+++ mfja/two_gears.py
@@ -1,7 +1,8 @@
 import numpy as np
 from pinocchio import SE3, neutral
 from pyhpp.constraints import (ComparisonType, ComparisonTypes, Implicit, Transformation)
-from pyhpp.core import (ConfigProjector, Discretized, Progressive, RandomShortcut)
+from pyhpp.core import (ConfigProjector, Discretized, ProgressiveProjector, Progressive,
+                        RandomShortcut)
 from pyhpp.manipulation import (Device, Graph, GraphPathValidation,
                                 Problem, ManipulationPlanner, SplineGradientBased_bezier3, urdf)
 from pyhpp.manipulation.constraint_graph_factory import ConstraintGraphFactory
@@ -52,6 +53,7 @@ robot.setJointBounds("gear_42_2/root_joint", [-1., 1.,
 problem = Problem(robot)
 problem.pathValidation(GraphPathValidation(Progressive(robot, .001)))
 problem.pathValidationFactory(GraphPathValidation(Progressive(robot, .001)))
+problem.pathProjector(ProgressiveProjector(problem.distance(), problem.steeringMethod(), 0.2))
 
 graph = Graph("robot", robot, problem)
 factory = ConstraintGraphFactory(graph)
@@ -69,7 +71,7 @@ factory.setPossibleGrasps({"staubli/tool0_gripper": ["gear_42_1/stud", "gear_42_
                            "gear_support/gear_42_1": ["gear_42_1/gear_support"],
                            "gear_support/gear_42_2": ["gear_42_2/gear_support"]})
 factory.generate()
-# Force linear motion when placing gear_42 on support
+# Force linear motion when placing gear_42_1 on support
 h = robot.handles()["gear_42_1/gear_support"]
 f = Transformation("vertical gear_42_1", robot, h.getParentJointId(), h.localPosition,
                    SE3.Identity(), [True, True, False, True, True, True])
@@ -81,6 +83,19 @@ transition = graph.getTransition("gear_support/gear_42_1 > gear_42_1/gear_suppor
 graph.addNumericalConstraintsToTransition(transition, [vertical_gear_42_1])
 transition = graph.getTransition("gear_support/gear_42_1 < gear_42_1/gear_support | 0-0:1-1_21")
 graph.addNumericalConstraintsToTransition(transition, [vertical_gear_42_1])
+
+# Force linear motion when placing gear_42_2 on support
+h = robot.handles()["gear_42_2/gear_support"]
+f = Transformation("vertical gear_42_2", robot, h.getParentJointId(), h.localPosition,
+                   SE3.Identity(), [True, True, False, True, True, True])
+cts = ComparisonTypes()
+cts[:] = [ComparisonType.Equality, ComparisonType.Equality, ComparisonType.Equality,
+          ComparisonType.Equality, ComparisonType.Equality]
+vertical_gear_42_2 = Implicit(f, cts, [True, True, True, True, True])
+transition = graph.getTransition("gear_support/gear_42_2 > gear_42_2/gear_support | 0-2:1-1_12")
+graph.addNumericalConstraintsToTransition(transition, [vertical_gear_42_2])
+transition = graph.getTransition("gear_support/gear_42_2 < gear_42_2/gear_support | 0-2:1-1:2-3_21")
+graph.addNumericalConstraintsToTransition(transition, [vertical_gear_42_2])
 
 # Deactive collision checking between gripper and gears for all transitions
 for transition in graph.getTransitions():
@@ -132,7 +147,7 @@ p = manipulationPlanner.solve()
 
 # Optimize the path
 opt1 = RandomShortcut(problem)
-opt1.maxIterations(1000)
+opt1.maxIterations(5000)
 p1 = opt1.optimize(p)
 
 toppra = Toppra(problem)
